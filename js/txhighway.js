8fca21357aa6b962139274f6dadf331ce19d1473
qbbt <52217807+qbbt@users.noreply.github.com>
2021-10-30 01:27:07 -0400

    Update txhighway.js

--- js/txhighway.js
+++ js/txhighway.js
@@ -304,9 +304,9 @@ function blockNotify(data, isCash){
 	// sets speed modifier for btc lane
 	let mod = t/amount/100;
 	if (mod >= 0){
-		SPEED_MODIFIER = .0010;
+		SPEED_MODIFIER = .0009;
 	} else {
-		SPEED_MODIFIER = .0010;
+		SPEED_MODIFIER = .0009;
 	}
 
 	setTimeout(() => {
